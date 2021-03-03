9515cfa532d84fe0e854bc9e3713bc6aee232b6e
ferhat44 <ferhat44@schule.bremen.de>
2021-03-03 08:29:06 +0100

    ...

---
+++ Konto/src/Adresse.java
@@ -0,0 +1,53 @@
+public class Adresse {
+
+    private String str;
+    private String hausNr;
+    private String pzl;
+    private String ort;
+
+    public Adresse(String str, String hausNr, String pzl, String ort) {
+        this.str = str;
+        this.hausNr = hausNr;
+        this.pzl = pzl;
+        this.ort = ort;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public String getHausNr() {
+        return hausNr;
+    }
+
+    public void setHausNr(String hausNr) {
+        this.hausNr = hausNr;
+    }
+
+    public String getPzl() {
+        return pzl;
+    }
+
+    public void setPzl(String pzl) {
+        this.pzl = pzl;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    @Override
+    public String toString() {
+        String text = "\n\tAdresse " + getStr() + " " + getHausNr();
+        text += "\n\t" + getPzl() + " " + getOrt();
+        return text;
+    }
+}
---
+++ Konto/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+
+    private String vorname;
+    private String nachname;
+    private Adresse adresse;
+
+
+}
