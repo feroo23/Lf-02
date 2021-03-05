b0b3e30b5fb3182981ac1dea5b9155cdcd9a1a9d
ferhat44 <ferhat44@schule.bremen.de>
2021-03-05 08:34:27 +0100

    ...

--- Konto/src/Programm.java
+++ Konto/src/Programm.java
@@ -1,6 +1,12 @@
 public class Programm {
 
     public static void main(String[] args) {
-        Kunde kunde = new Kunde("fero", "gümüs", "slm", "ghfjg");
+        Adresse adresseKunde = new Adresse("Amsterdammer str", "16", "28259", "Bremen");
+        Adresse adresseMit = new Adresse("Kirchhuchtinger Land str", "165A", "28259", "Bremen");
+        Mitarbeiter mitarbeiter = new Mitarbeiter("Frag","Nicht",adresseMit);
+        Kunde kunde = new Kunde("Fero", "23", adresseKunde,mitarbeiter);
+
+        System.out.println(Mitarbeiter.to);
+        Mitarbeiter.to
     }
 }
